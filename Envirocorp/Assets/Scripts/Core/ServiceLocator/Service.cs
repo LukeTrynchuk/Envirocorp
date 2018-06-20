151b310aa60065cec347fd151765629f32151bf8
Unknown <luke123t@gmail.com>
2018-06-20 20:50:56 -0300

    Add Service Locator System

---
+++ Envirocorp/Assets/Scripts/Core/ServiceLocator/Service.cs
@@ -0,0 +1,12 @@
+﻿namespace FireBullet.Enviro.Core.Services
+{
+    /// <summary>
+    /// A service should be implemented
+    /// by any other interface that is
+    /// meant to be used as a service.
+    /// </summary>
+    public interface IService 
+	{
+		void RegisterService ();
+	}
+}
