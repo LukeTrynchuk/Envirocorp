27b697bec89224448e757b864f16008f43d26830
Unknown <luke123t@gmail.com>
2018-06-20 21:57:30 -0300

    Add Hex Prefab

---
+++ Envirocorp/Assets/Scripts/Board/HexCell.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace FireBullet.Enviro.Board
+{
+    /// <summary>
+    /// A hex cell represents a single unit on 
+    /// the board. A hex cell contains data about
+    /// what kind of resources can be found on the
+    /// hex.
+    /// </summary>
+    public class HexCell : MonoBehaviour 
+    {
+    }
+}
