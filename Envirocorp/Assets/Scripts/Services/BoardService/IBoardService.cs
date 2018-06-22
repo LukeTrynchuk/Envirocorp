338ce0d1116383801191730470d2084744e7ab12
Unknown <luke123t@gmail.com>
2018-06-21 22:55:58 -0300

    Add Board Service

---
+++ Envirocorp/Assets/Scripts/Services/BoardService/IBoardService.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using FireBullet.Enviro.Board;
+using FireBullet.Core.Services;
+
+namespace FireBullet.Enviro.Services
+{
+    /// <summary>
+    /// The IBoardService is a contract that all
+    /// boards must implement. A board service
+    /// is responsible for keeping reference
+    /// to the board.
+    /// </summary>
+    public interface IBoardService : IService
+    {
+        int Width { get; }
+        int Height { get; }
+        HexCell[] GetBoard();
+        HexCell GetCellAt(HexCoordinate coordinate);
+    }
+}
