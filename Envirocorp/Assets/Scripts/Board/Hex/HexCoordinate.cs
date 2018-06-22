fd510c6dd396b592fdb8bf92b212188ecc06f8b2
Unknown <luke123t@gmail.com>
2018-06-21 21:05:17 -0300

    Add Hex Coordinate Struct

---
+++ Envirocorp/Assets/Scripts/Board/Hex/HexCoordinate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace FireBullet.Enviro.Board
+{
+    /// <summary>
+    /// The Hex Coordinate struct is meant to 
+    /// be used as a data structure to represent
+    /// the coordinate value of a single hex.
+    /// </summary>
+    [System.Serializable]
+    public struct HexCoordinate
+    {
+        public int X { get; private set; }
+        public int Z { get; private set; }
+
+        public HexCoordinate(int x, int z)
+        {
+            X = x;
+            Z = z;
+        }
+
+        public static HexCoordinate FromOffsetCoordinates(int x, int z) => new HexCoordinate(x, z);
+        public override string ToString() => $"{X.ToString()},{Z.ToString()}";
+        public string ToStringOnSeparateLines() => $"{X.ToString()}\n{Z.ToString()}";
+    }
+}
