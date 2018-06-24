b96dbc5fea9dc16dca1db17f3fa0be67f819c0b0
Unknown <luke123t@gmail.com>
2018-06-23 21:44:41 -0300

    Add omitting backslash input

---
+++ Envirocorp/Assets/Scripts/UI/OmitCharOnTMProInputField.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace FireBullet.Enviro.UI
+{
+    /// <summary>
+    /// OmitCharOnTMProText will omit a specified
+    /// char from a text mesh pro components text.
+    /// </summary>
+    public class OmitCharOnTMProInputField : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField]
+        private TMP_InputField m_inputField;
+
+        [SerializeField]
+        private char m_charToOmit;
+		#endregion
+
+		#region Main Methods
+		private void Start()
+		{
+            m_inputField.onValidateInput += delegate (string input, int charIndex, char addedChar) { return ProcessText(addedChar); };
+		}
+
+		public char ProcessText(char charToValidate)
+        {
+            if (charToValidate.Equals(m_charToOmit)) return ' ';
+            return charToValidate;
+        }
+        #endregion
+    }
+}
