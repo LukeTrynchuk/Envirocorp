9be9ef061593c6a303797f630127e8c3c2a2f9ae
Unknown <luke123t@gmail.com>
2018-06-24 15:33:01 -0300

    Add Neighbor arrays and setup

--- Envirocorp/Assets/Scripts/Board/Hex/HexCell.cs
+++ Envirocorp/Assets/Scripts/Board/Hex/HexCell.cs
@@ -8,14 +8,30 @@ namespace FireBullet.Enviro.Board
     /// what kind of resources can be found on the
     /// hex.
     /// </summary>
-    public class HexCell : MonoBehaviour 
+    public class HexCell : MonoBehaviour
     {
+        #region Public Variables
         public HexCoordinate m_Coordinate;
         public Color m_Color;
+        #endregion
 
-		private void Awake()
-		{
+        #region Private Variables
+        [SerializeField]
+        private HexCell[] m_neighbours;
+        #endregion
+
+        #region Main Methods
+        private void Awake()
+        {
             m_Color = Color.white;
-		}
-	}
+        }
+
+        public HexCell GetNeighbour(HexDirection direction) => m_neighbours[(int)direction];
+        public void SetNeighbour(HexDirection direction, HexCell cell) 
+        {
+			m_neighbours[(int)direction] = cell;
+            cell.m_neighbours[(int)direction.Opposite()] = this;
+        }
+        #endregion
+    }
 }
