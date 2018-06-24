d1ded9ef9f3babf19feef4280248920676811414
Unknown <luke123t@gmail.com>
2018-06-24 00:26:09 -0300

    Add Hex Definition Service and Hex Button Creation

---
+++ Envirocorp/Assets/Scripts/Services/HexDefinitionService/HexDefinitionService.cs
@@ -0,0 +1,30 @@
+﻿using FireBullet.Enviro.Board;
+using UnityEngine;
+using FireBullet.Core.Services;
+
+namespace FireBullet.Core.Services
+{
+    /// <summary>
+    /// Hex Definition service is responsible for 
+    /// providing all hex definitions to other services
+    /// and systems.
+    /// </summary>
+    public class HexDefinitionService : MonoBehaviour, IHexDefinitionService
+    {
+        #region Private Variables
+        [SerializeField]
+        HexTypeDefinition[] m_definitions;
+        #endregion
+
+        #region Main Methods
+        public HexTypeDefinition[] GetHexDefinitions() => m_definitions;
+
+        void Start() => RegisterService();
+
+        public void RegisterService()
+        {
+            ServiceLocator.Register<IHexDefinitionService>(this);
+        }
+        #endregion
+    }
+}
---
+++ Envirocorp/Assets/Scripts/Services/MapEditorService/Utilities/CreateHexTypeButtons.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using FireBullet.Core.Services;
+using FireBullet.Enviro.Services;
+using FireBullet.Enviro.Board;
+using UnityEngine.UI;
+
+namespace FireBullet.Enviro.UI
+{
+    /// <summary>
+    /// Creates the required number of hex type buttons
+    /// for use in the Map Editor UI.
+    /// </summary>
+    public class CreateHexTypeButtons : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField]
+        private GameObject m_hexButtonPrefab;
+
+        private ServiceReference<IHexDefinitionService> m_hexDefinitionService
+                                = new ServiceReference<IHexDefinitionService>();
+        #endregion
+
+        #region Main Methods
+        private void Start()
+        {
+            m_hexDefinitionService.AddRegistrationHandle(HandleHexDefinitionServiceRegistered);
+        }
+
+        private void CreateButtons(HexTypeDefinition[] definitions)
+        {
+            foreach(HexTypeDefinition definition in definitions)
+            {
+                GameObject button = Instantiate(m_hexButtonPrefab);
+                button.transform.parent = transform;
+                Image image = button.GetComponent<Image>();
+                image.color = definition.TypeColor;
+            }
+        }
+        #endregion
+
+        #region Utility Methods
+        private void HandleHexDefinitionServiceRegistered()
+        {
+            CreateButtons(m_hexDefinitionService.Reference.GetHexDefinitions());
+        }
+        #endregion
+    }
+}
